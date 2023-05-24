9831400e17886d1480d384cd8bdf726ecccdbb76
pc 21 <15pc21@upqroo.edu.mx>
2023-05-24 17:20:22 -0500

    csddsf

---
+++ Music/majooo/Poo-Estructura (1)/Poo-Estructura/Program.cs
@@ -0,0 +1,27 @@
+﻿using Poo_Estructura.Entities;
+using Poo_Estructura.Servicios;
+using System;
+
+namespace Poo_Estructura
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+            
+        {
+
+
+            AlumnoServicio servicio = new AlumnoServicio();
+
+            Alumno alumno1 = servicio.Agregar();
+          
+            //var res = servicio.Agregar();
+            servicio.Imprimir(alumno1);
+            //creando una variable general 
+
+
+            Console.WriteLine("la calificacion es"+servicio.ValidarProm(alumno1.Promedio));
+
+        }
+    }
+}
---
+++ Music/majooo/Poo-Estructura (1)/Poo-Estructura/Servicios/AlumnoServicio.cs
@@ -0,0 +1,68 @@
+﻿using Poo_Estructura.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poo_Estructura.Servicios
+{
+    public class AlumnoServicio
+    {
+        public Alumno Agregar()
+        {
+            try
+            {
+                Alumno alumno = new Alumno()
+                {
+                    Id = 1,
+                    Nombre = "Beto Estrada",
+                    Apellido = "Canto",
+                    FechaIngreso = DateTime.Now,
+                    Grado = 23,
+                    Promedio = 5
+                   
+                };
+                return alumno;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error" + ex.Message);
+
+            }
+
+
+
+        }
+        public void Imprimir(Alumno request)
+        {
+            try
+            {
+                Console.WriteLine("El ID del alumno es de: " + request.Id);
+                Console.WriteLine("El nombre es: " + request.Nombre);
+                Console.WriteLine("El Apellido es: " + request.Apellido);
+                Console.WriteLine("La fecha de ingreso es de: " + request.FechaIngreso);
+                Console.WriteLine("El grado que cursa es: " + request.Grado);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error" + ex.Message);
+
+            }
+        }
+
+        #region----- Validar Aprobacion 
+        public bool ValidarProm(double num1)
+        {
+            bool validar = false;
+
+            if(num1 > 7)
+            {
+                validar = true; 
+            }
+            return validar;
+        }
+        #endregion
+    }
+}
+
